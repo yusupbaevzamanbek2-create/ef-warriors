95f50e62f478aa664cb154355ecb8d0462a1c76b
yusupbaevzamanbek2-create <yusupbaevzamanbek2@gmail.com>
2026-06-26 09:03:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,5 @@
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(
-        message.chat.id,
-        "Salom!",
-        reply_markup=types.ReplyKeyboardRemove()
-    )
     inline = types.InlineKeyboardMarkup()
     inline.add(
         types.InlineKeyboardButton("🇺🇿 O'zbekcha", callback_data="uz"),
@@ -13,6 +8,6 @@ def start(message):
     )
     bot.send_message(
         message.chat.id,
-        "Tilni tanlang / Выберите язык / Choose language:",
+        "Salom! 👋\n\nTilni tanlang / Выберите язык / Choose language:",
         reply_markup=inline
     )
